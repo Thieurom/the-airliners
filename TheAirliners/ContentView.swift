08b19d32c2f8b3ffaf26b2eee203291242b07af5
Doan Le Thieu <thieurom@gmail.com>
2019-12-31 23:33:42 +0700

    Initial commit

---
+++ TheAirliners/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheAirliners
+//
+//  Created by Doan Le Thieu on 12/31/19.
+//  Copyright © 2019 Doan Le Thieu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
